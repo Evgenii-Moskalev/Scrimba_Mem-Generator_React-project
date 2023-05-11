391e0446766b06b4d1d184f0fdb5183bc0786326
Evgenii Moskalev <evgenii.moskalev.cohort2211@gmail.com>
2023-05-10 22:47:48 -0700

    Fix: add some styles

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -37,7 +37,11 @@ export default function Meme() {
 
                 <button className="form--button" onClick={getMemeImage}>Get a new meme image  🖼</button>
             </div>
-            <img src={meme.randomImage} alt="Meme" className="meme--image" />
+            <div className="meme">
+                <img src={meme.randomImage} className="meme--image" alt="Random Meme" />
+                <h2 className="meme--text top">One does not simply</h2>
+                <h2 className="meme--text bottom">Walk into Mordor</h2>
+            </div>
         </main>
     )
 }
