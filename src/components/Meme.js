59082fa90b04fa4c16d06f9bad8463efa6291697
Evgenii Moskalev <evgenii.moskalev.cohort2211@gmail.com>
2023-05-11 18:23:39 -0700

    Feature: auto-adding text above meme-image when typing in input field.

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -15,6 +15,17 @@ export default function Meme() {
     const [allMemeImages, setAllMemeImages] = React.useState(MemeDatabase);
     // console.log(allMemeImages);
 
+    function handleChange(event) {
+        const { name, value } = event.target;
+
+        setMeme((prevState) => {
+            return {
+                ...meme,
+                [name]: value
+            }
+        })
+    }
+
     function getMemeImage() {
         const randomNum = Math.floor(Math.random() * (MemeDatabase.data.memes.length));
         // console.log(randomNum);
@@ -28,19 +39,30 @@ export default function Meme() {
         }))
     }
 
-
     return (
         <main>
             <div className="form">
-                <input type="text" placeholder="Top text" className="form--input"></input>
-                <input type="text" placeholder="Bottom text" className="form--input"></input>
+                <input type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleChange}
+                ></input>
+                <input type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}
+                ></input>
 
                 <button className="form--button" onClick={getMemeImage}>Get a new meme image  🖼</button>
             </div>
             <div className="meme">
                 <img src={meme.randomImage} className="meme--image" alt="Random Meme" />
-                <h2 className="meme--text top">One does not simply</h2>
-                <h2 className="meme--text bottom">Walk into Mordor</h2>
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
             </div>
         </main>
     )
